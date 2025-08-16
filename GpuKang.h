e4f0f31e32d531aaaaa38599b71eb4afc569fa1d
morgansaul <morgansaul00@gmail.com>
2025-08-16 07:34:55 -0400

    Update GpuKang.h

--- GpuKang.h
+++ GpuKang.h
@@ -1,75 +1,81 @@
-// This file is a part of RCKangaroo software
-// (c) 2024, RetiredCoder (RC)
-// License: GPLv3, see "LICENSE.TXT" file
-// https://github.com/RetiredC
-
-
 #pragma once
 
 #include "Ec.h"
+#include "defs.h"
 
-#define STATS_WND_SIZE	16
+#define STATS_WND_SIZE    16
 
 struct EcJMP
 {
-	EcPoint p;
-	EcInt dist;
+    EcPoint p;
+    EcInt dist;
 };
 
-//96bytes size
+// 96 bytes size
 struct TPointPriv
 {
-	u64 x[4];
-	u64 y[4];
-	u64 priv[4];
+    u64 x[4];
+    u64 y[4];
+    u64 priv[4];
 };
 
+// [MODIFIED] Extended GPU kangaroo class
 class RCGpuKang
 {
 private:
-	bool StopFlag;
-	EcPoint PntToSolve;
-	int Range; //in bits
-	int DP; //in bits
-	Ec ec;
+    bool StopFlag;
+    EcPoint* PntsToSolve;  // Changed from single point to array
+    int PubKeyCount;       // Number of loaded pubkeys
+    int Range;            // in bits
+    int DP;               // in bits
+    Ec ec;
 
-	u32* DPs_out;
-	TKparams Kparams;
+    u32* DPs_out;
+    TKparams Kparams;
 
-	EcInt HalfRange;
-	EcPoint PntHalfRange;
-	EcPoint NegPntHalfRange;
-	TPointPriv* RndPnts;
-	EcJMP* EcJumps1;
-	EcJMP* EcJumps2;
-	EcJMP* EcJumps3;
+    EcInt HalfRange;
+    EcPoint PntHalfRange;
+    EcPoint NegPntHalfRange;
+    TPointPriv* RndPnts;
+    EcJMP* EcJumps1;
+    EcJMP* EcJumps2;
+    EcJMP* EcJumps3;
 
-	EcPoint PntA;
-	EcPoint PntB;
+    EcPoint* PntA;        // Arrays for each pubkey
+    EcPoint* PntB;
+    
+    int cur_stats_ind;
+    int SpeedStats[STATS_WND_SIZE];
 
-	int cur_stats_ind;
-	int SpeedStats[STATS_WND_SIZE];
-
-	void GenerateRndDistances();
-	bool Start();
-	void Release();
+    void GenerateRndDistances();
+    bool Start();
+    void Release();
 #ifdef DEBUG_MODE
-	int Dbg_CheckKangs();
+    int Dbg_CheckKangs();
 #endif
+
 public:
-	int persistingL2CacheMaxSize;
-	int CudaIndex; //gpu index in cuda
-	int mpCnt;
-	int KangCnt;
-	bool Failed;
-	bool IsOldGpu;
+    int persistingL2CacheMaxSize;
+    int CudaIndex; // GPU index in cuda
+    int mpCnt;
+    int KangCnt;
+    bool Failed;
+    bool IsOldGpu;
 
-	int CalcKangCnt();
-	bool Prepare(EcPoint _PntToSolve, int _Range, int _DP, EcJMP* _EcJumps1, EcJMP* _EcJumps2, EcJMP* _EcJumps3);
-	void Stop();
-	void Execute();
+    int CalcKangCnt();
+    
+    // [MODIFIED] Updated Prepare() for multiple pubkeys
+    bool Prepare(EcPoint* _PntsToSolve, int _PubKeyCount, int _Range, int _DP, 
+                EcJMP* _EcJumps1, EcJMP* _EcJumps2, EcJMP* _EcJumps3);
+                
+    void Stop();
+    void Execute();
 
-	u32 dbg[256];
+    u32 dbg[256];
 
-	int GetStatsSpeed();
+    int GetStatsSpeed();
 };
+
+// [ADDED] Helper functions for GPU operations
+__device__ bool CheckPointAgainstAllTargets(const EcPoint& point, const TPubKey* targets, int count);
+__device__ int FindMatchingPubKey(const EcPoint& point, const TPubKey* targets, int count);
