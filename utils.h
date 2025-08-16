f1a6dca8bee44988c22b5b7ccd5c3930322483f4
morgansaul <morgansaul00@gmail.com>
2025-08-16 07:32:14 -0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,46 +1,36 @@
-// This file is a part of RCKangaroo software
-// (c) 2024, RetiredCoder (RC)
-// License: GPLv3, see "LICENSE.TXT" file
-// https://github.com/RetiredC
-
-
 #pragma once
 
 #include <string.h>
 #include <stdio.h>
 #include <vector>
+#include <fstream>
 #include "defs.h"
 
 #ifdef _WIN32
-
-	#include <Windows.h>
-	#include <process.h>
-	#include <intrin.h>
-
-	#define CSHANDLER		CRITICAL_SECTION
-	#define INIT_CS(cs)     InitializeCriticalSection((cs))
-	#define DELETE_CS(cs)   DeleteCriticalSection((cs))
-	#define LOCK_CS(cs)     EnterCriticalSection((cs))
-	#define TRY_LOCK_CS(cs)
-	#define UNLOCK_CS(cs)   LeaveCriticalSection((cs))
-
-	#define HHANDLER		HANDLE
-
+    #include <Windows.h>
+    #include <process.h>
+    #include <intrin.h>
+    #define CSHANDLER       CRITICAL_SECTION
+    #define INIT_CS(cs)     InitializeCriticalSection((cs))
+    #define DELETE_CS(cs)   DeleteCriticalSection((cs))
+    #define LOCK_CS(cs)     EnterCriticalSection((cs))
+    #define TRY_LOCK_CS(cs)
+    #define UNLOCK_CS(cs)   LeaveCriticalSection((cs))
+    #define HHANDLER        HANDLE
 #else
-	#include <math.h>
-	#include <pthread.h>
-	#include <unistd.h>
-	#include <x86intrin.h>
-	#define DWORD           u32
-	#define CSHANDLER		pthread_mutex_t
-	#define INIT_CS(cs)		{pthread_mutexattr_t attr; pthread_mutexattr_init(&attr); pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE); pthread_mutex_init((cs), &attr);}
-	#define DELETE_CS(cs)	pthread_mutex_destroy((cs))
-	#define LOCK_CS(cs)		pthread_mutex_lock((cs))
-	#define UNLOCK_CS(cs)	pthread_mutex_unlock((cs))
-	#define HHANDLER		pthread_t
- 
-	u64 GetTickCount64();
-	static void Sleep(int x) { usleep(x * 1000); }      
+    #include <math.h>
+    #include <pthread.h>
+    #include <unistd.h>
+    #include <x86intrin.h>
+    #define DWORD           u32
+    #define CSHANDLER       pthread_mutex_t
+    #define INIT_CS(cs)     {pthread_mutexattr_t attr; pthread_mutexattr_init(&attr); pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE); pthread_mutex_init((cs), &attr);}
+    #define DELETE_CS(cs)   pthread_mutex_destroy((cs))
+    #define LOCK_CS(cs)     pthread_mutex_lock((cs))
+    #define UNLOCK_CS(cs)   pthread_mutex_unlock((cs))
+    #define HHANDLER        pthread_t
+    u64 GetTickCount64();
+    static void Sleep(int x) { usleep(x * 1000); }      
     void _BitScanReverse64(u32* index, u64 msk);
     void _BitScanForward64(u32* index, u64 msk);       
     typedef __uint128_t uint128_t;
@@ -49,58 +39,98 @@
     u64 __shiftleft128 (u64 LowPart, u64 HighPart, u8 Shift);
 #endif
 
-class CriticalSection
-{
+class CriticalSection {
 private:
-	CSHANDLER cs_body;
+    CSHANDLER cs_body;
 public:
-	CriticalSection() { INIT_CS(&cs_body); };
-	~CriticalSection() { DELETE_CS(&cs_body); };
-
-	void Enter() { LOCK_CS(&cs_body); };
-	void Leave() { UNLOCK_CS(&cs_body); };
+    CriticalSection() { INIT_CS(&cs_body); };
+    ~CriticalSection() { DELETE_CS(&cs_body); };
+    void Enter() { LOCK_CS(&cs_body); };
+    void Leave() { UNLOCK_CS(&cs_body); };
 };
 
 #pragma pack(push, 1)
-struct TListRec
-{
-	u16 cnt;
-	u16 capacity;
-	u32* data;
+struct TListRec {
+    u16 cnt;
+    u16 capacity;
+    u32* data;
 };
 #pragma pack(pop)
 
-class MemPool
-{
+class MemPool {
 private:
-	std::vector <void*> pages;
-	u32 pnt;
+    std::vector<void*> pages;
+    u32 pnt;
 public:
-	MemPool();
-	~MemPool();
-	void Clear();
-	inline void* AllocRec(u32* cmp_ptr);
-	inline void* GetRecPtr(u32 cmp_ptr);
+    MemPool();
+    ~MemPool();
+    void Clear();
+    inline void* AllocRec(u32* cmp_ptr);
+    inline void* GetRecPtr(u32 cmp_ptr);
 };
 
-class TFastBase
-{
+// [ADD] Multi-pubkey container
+class PubKeyContainer {
 private:
-	MemPool mps[256];
-	TListRec lists[256][256][256];
-	int lower_bound(TListRec* list, int mps_ind, u8* data);
+    std::vector<TPubKey> keys;
+    CriticalSection cs;
 public:
-	u8 Header[256];
+    bool LoadFromFile(const char* path);
+    int Count() const { return (int)keys.size(); }
+    const TPubKey* Data() const { return keys.data(); }
+    void Clear() { keys.clear(); }
+};
 
-	TFastBase();
-	~TFastBase();
-	void Clear();
-	u8* AddDataBlock(u8* data, int pos = -1);
-	u8* FindDataBlock(u8* data);
-	u8* FindOrAddDataBlock(u8* data);
-	u64 GetBlockCnt();
-	bool LoadFromFile(char* fn);
-	bool SaveToFile(char* fn);
+class TFastBase {
+private:
+    MemPool mps[256];
+    TListRec lists[256][256][256];
+    PubKeyContainer pubkeys; // [ADD] Multi-pubkey storage
+    int lower_bound(TListRec* list, int mps_ind, u8* data);
+public:
+    u8 Header[256];
+
+    TFastBase();
+    ~TFastBase();
+    void Clear();
+    u8* AddDataBlock(u8* data, int pos = -1);
+    u8* FindDataBlock(u8* data);
+    u8* FindOrAddDataBlock(u8* data);
+    u64 GetBlockCnt();
+    bool LoadFromFile(char* fn);
+    bool SaveToFile(char* fn);
+
+    // [ADD] Pubkey management
+    bool LoadPubkeys(const char* path) { return pubkeys.LoadFromFile(path); }
+    int PubkeyCount() const { return pubkeys.Count(); }
+    const TPubKey* GetPubkeys() const { return pubkeys.Data(); }
 };
 
 bool IsFileExist(char* fn);
+
+// [ADD] New functions for pubkey loading
+inline bool ParsePubKeyLine(const char* line, TPubKey& key) {
+    EcPoint p;
+    if (!p.SetHexStr(line)) return false;
+    memcpy(key.x, p.x.data, 32);
+    memcpy(key.y, p.y.data, 32);
+    return true;
+}
+
+inline bool PubKeyContainer::LoadFromFile(const char* path) {
+    std::ifstream file(path);
+    if (!file) return false;
+    
+    Clear();
+    std::string line;
+    TPubKey key;
+    LOCK_CS(&cs_body);
+    while (std::getline(file, line)) {
+        if (ParsePubKeyLine(line.c_str(), key)) {
+            keys.push_back(key);
+            if (keys.size() >= MAX_PUBKEYS) break;
+        }
+    }
+    UNLOCK_CS(&cs_body);
+    return !keys.empty();
+}
